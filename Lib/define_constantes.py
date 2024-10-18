c2cb9e623b9153d582c7fae8d39dae529c477465
dsance <70341612+dsance@users.noreply.github.com>
2024-10-18 14:58:12 -0500

    Add files via upload
    Carga inicial

---
+++ Lib/define_constantes.py
@@ -0,0 +1,8 @@
+# Definición de constantes para el proyecto
+
+DIRECTORIO_BASE       = '..'
+DIRECTORIO_DATOS      = f'{DIRECTORIO_BASE}/Datos'
+DIRECTORIO_RESULTADOS = f'{DIRECTORIO_BASE}/Resultados'
+DIRECTORIO_MODELOS    = f'{DIRECTORIO_BASE}/Modelos'
+DIRECTORIO_LIB        = f'{DIRECTORIO_BASE}/Lib'
+DIRECTORIO_GRAFICAS   = f'{DIRECTORIO_BASE}/Gráficas'
